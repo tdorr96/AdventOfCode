0db7fa4675d42483d8a7838adfbfab35ab75ef3e
tdorr96 <thomas.dorrington@cantab.net>
2025-04-15 11:53:52 +0100

    day 1

--- 2024/day_1.py
+++ 2024/day_1.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 
 def part_one():
 
-    # Much more efficient to maintain a sorted list as we build them, as opposed to sorting at the end
+    # Much more efficient to maintain a sorted list as we build it, as opposed to sorting at the end
     # The time gains are minimal, as it's such a small file anyway, but good practice!
     left, right = [], []
     with open('day_1_input.txt', 'r') as open_f:
@@ -18,7 +18,7 @@ def part_one():
 
 def part_two():
 
-    # Again, consider the best data structure to answer the question WHEN building the list
+    # Again, consider the best data structure to answer the question as we're building the 'list'
     # Rather, than loading in as a list, and working with it after that
     # Left & right numbers represented as a dictionary, mapping from number to count
     # We use a default dict for syntax simplicity, but also important when indexing during calculation after that
@@ -35,5 +35,5 @@ def part_two():
 
 if __name__ == '__main__':
 
-    # part_one()
+    part_one()
     part_two()
